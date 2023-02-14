9a30ff5e53cb421076476f5396b26f5ba3b74a98
JokerBoyCP <claudio.pacheco98@hotmail.com>
2023-02-14 13:52:04 +0100

    creation of the base

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<header>
+    <nav>
+        <a href="#">
+            <img src="img/listiify.png" alt="logo">
+        </a>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+                <a href="#">To Do</a>
+                <a href="#">Account</a>
+            </li>
+        </ul>
+        <div>
+            <form action="includes/login.php" method="post">
+                <input type="text" name="mailuid" placeholder="Username/Email...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            a
+
+        </div>
+
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+require "header.php";
+
+?>
+
+<main>
+
+
+</main>
+
+<?php 
+
+require "footer.php";
+
+?>
