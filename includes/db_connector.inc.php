b31f84a3545b13673396c3c22b7b37e5e6896af9
JokerBoyCP <99121408+JokerBoyCP@users.noreply.github.com>
2023-02-16 13:19:21 +0100

    db connector

---
+++ includes/db_connector.inc.php
@@ -0,0 +1,17 @@
+// Variabeln deklarieren
+$host = 'localhost'; // host
+$username = 'listiify'; // username
+$password = 'BBZBL12345'; // password
+$database = 'listiify_db'; // database
+
+
+
+// mit der Datenbank verbinden
+$mysqli = new mysqli($host, $username, $password, $database);
+
+
+
+// Fehlermeldung, falls Verbindung fehl schlägt.
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+}
