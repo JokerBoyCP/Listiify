30cbf7564791e370566811f9ad2d7e8de96dab42
JokerBoyCP <99121408+JokerBoyCP@users.noreply.github.com>
2023-04-20 13:11:27 +0200

    update

--- listiify_db.sql
+++ listiify_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 16. Mrz 2023 um 15:55
+-- Erstellungszeit: 20. Apr 2023 um 12:46
 -- Server-Version: 10.4.24-MariaDB
 -- PHP-Version: 8.1.6
 
@@ -24,26 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `lists`
---
-
-CREATE TABLE `lists` (
-  `list_id` int(11) NOT NULL,
-  `idUsers` int(11) NOT NULL,
-  `list_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Tabellenstruktur für Tabelle `tasks`
 --
 
 CREATE TABLE `tasks` (
   `task_id` int(11) NOT NULL,
-  `list_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `task` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `tasks`
+--
+
+INSERT INTO `tasks` (`task_id`, `user_id`, `task`) VALUES
+(1, 1, 'test'),
+(2, 13, 'Testsdf'),
+(3, 13, 'Testdskjd');
 
 -- --------------------------------------------------------
 
@@ -55,7 +52,7 @@ CREATE TABLE `users` (
   `idUsers` int(11) NOT NULL,
   `uidUsers` varchar(30) NOT NULL,
   `emailUsers` varchar(80) NOT NULL,
-  `pwdUsers` varchar(40) NOT NULL
+  `pwdUsers` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -67,25 +64,18 @@ INSERT INTO `users` (`idUsers`, `uidUsers`, `emailUsers`, `pwdUsers`) VALUES
 (4, 'JokerBoyCP', 'claudio.pacheco98@hotmail.com', '$2y$10$yMAB1SyB3FfdEA6.a.3JtutYQ1BQ6zL6a'),
 (5, 'Jannick', 'Jannick@gmail.com', '$2y$10$KV3RnE0GVBcxvdL5g7AAnOa4mNL0TK2OZ'),
 (6, 'daniel', 'd@b.ch', '$2y$10$8fzJ93znJpYjWVarEFUP1Ourd6/zxma49'),
-(8, 'claudio', 'c.p@c.com', '$2y$10$rLQ9YPZQrTeNQxOKjtnzieDtwBKwKyTnL');
+(12, 'g', 'g@g.ch', '$2y$10$wdBh1E6RWB2r8MrHxtR1yOQoWw6//Xesz'),
+(13, 'Test1', 'test@t.ch', '$2y$10$7XwJLCFA8JUD8i/VW6qSYuSiYYscoJHumq3x3lxoYdp5wIQYjAPjW');
 
 --
 -- Indizes der exportierten Tabellen
 --
 
 --
--- Indizes für die Tabelle `lists`
---
-ALTER TABLE `lists`
-  ADD PRIMARY KEY (`list_id`),
-  ADD KEY `idUsers` (`idUsers`);
-
---
 -- Indizes für die Tabelle `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`task_id`),
-  ADD KEY `list_id` (`list_id`);
+  ADD PRIMARY KEY (`task_id`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -98,38 +88,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT für Tabelle `lists`
---
-ALTER TABLE `lists`
-  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT für Tabelle `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `lists`
---
-ALTER TABLE `lists`
-  ADD CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`idUsers`) REFERENCES `users` (`idUsers`);
-
---
--- Constraints der Tabelle `tasks`
---
-ALTER TABLE `tasks`
-  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`list_id`) REFERENCES `lists` (`list_id`);
+  MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
